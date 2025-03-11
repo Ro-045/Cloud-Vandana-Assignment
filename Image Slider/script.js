b85f7989a4dae83552514694c117109631460d19
Ro-045 <pratapnaidu016@gmail.com>
2025-03-11 12:54:51 +0530

    add more images

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -3,7 +3,12 @@ const images = [
     "images/img2.jpg",
     "images/img3.jpg",
     "images/img4.jpg",
-    "images/img5.jpg"
+    "images/img5.jpg",
+    "images/img6.jpg",
+    "images/img7.jpg",
+    "images/img8.jpg",
+    "images/img9.jpg",
+    "images/img10.jpg"
 ];
 
 let currentIndex = 0;
