e2e794f82b375287c386aee8ba0d00f85bb13395
Ro-045 <pratapnaidu016@gmail.com>
2025-03-11 12:17:13 +0530

    image slider

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -2,7 +2,8 @@ const images = [
     "images/img1.jpg",
     "images/img2.jpg",
     "images/img3.jpg",
-    "images/img4.jpg"
+    "images/img4.jpg",
+    "images/img5.jpg"
 ];
 
 let currentIndex = 0;
