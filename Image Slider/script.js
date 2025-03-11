4afaea411cdbe6e9f25ded374e6e552369f60101
Ro-045 <pratapnaidu016@gmail.com>
2025-03-11 12:15:04 +0530

    image slider

---
+++ Image Slider/script.js
@@ -0,0 +1,30 @@
+const images = [
+    "images/img1.jpg",
+    "images/img2.jpg",
+    "images/img3.jpg",
+    "images/img4.jpg"
+];
+
+let currentIndex = 0;
+const sliderImage = document.getElementById("sliderImage");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+function updateImage() {
+    sliderImage.src = images[currentIndex];
+}
+
+
+updateImage();
+
+
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length; 
+    updateImage();
+});
+
+
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length; 
+    updateImage();
+});
